78fd8f1cf7cbecb48bc8236f1ed58357c2391d02
Jose G <bonustrack310@gmail.com>
2017-09-10 09:17:30 -0500

    Add supergluu support

---
+++ app/src/main/java/org/gluu/credmanager/core/credential/SuperGluuDevice.java
@@ -0,0 +1,28 @@
+package org.gluu.credmanager.core.credential;
+
+import org.gluu.credmanager.core.credential.fido.FidoDevice;
+import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
+import org.gluu.site.ldap.persistence.annotation.LdapJsonObject;
+import org.xdi.oxauth.model.fido.u2f.protocol.DeviceData;
+
+/**
+ * Created by jgomer on 2017-09-06.
+ * Represents a registered credential corresponding to a supergluu device
+ */
+public class SuperGluuDevice extends FidoDevice {
+
+    public SuperGluuDevice(){}
+
+    @LdapJsonObject
+    @LdapAttribute(name = "oxDeviceData")
+    private DeviceData deviceData;
+
+    public DeviceData getDeviceData() {
+        return deviceData;
+    }
+
+    public void setDeviceData(DeviceData deviceData) {
+        this.deviceData = deviceData;
+    }
+
+}
