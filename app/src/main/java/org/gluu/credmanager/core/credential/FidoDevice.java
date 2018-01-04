d4944678362e2b1d6be90b373c55b5ce93b06018
Jose G <bonustrack310@gmail.com>
2018-01-04 10:05:40 -0500

    Add MIT license where missing

--- app/src/main/java/org/gluu/credmanager/core/credential/FidoDevice.java
+++ app/src/main/java/org/gluu/credmanager/core/credential/FidoDevice.java
@@ -1,3 +1,8 @@
+/*
+ * cred-manager is available under the MIT License 2008. See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright c 2017, Gluu
+ */
 package org.gluu.credmanager.core.credential;
 
 import org.gluu.site.ldap.persistence.annotation.LdapAttribute;
